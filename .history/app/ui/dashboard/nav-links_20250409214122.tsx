1e8ed289d698a2b2d146d0e06e1f88399a249439
Sabrina Müller <schprini@yahoo.de>
2025-04-09 21:48:46 +0200

    initial commit

--- .history/app/ui/dashboard/nav-links_20250409214122.tsx
+++ .history/app/ui/dashboard/nav-links_20250409214122.tsx
@@ -6,7 +6,7 @@ import {
     DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathName } from 'next/navigation';
 import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
@@ -22,7 +22,7 @@ const links = [
 ];
 
 export default function NavLinks() {
-    const pathName = usePathname();
+    const pathName = usePathName();
     return (
         <>
             {links.map((link) => {
